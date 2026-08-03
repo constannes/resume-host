00a77b1fb356cf90975071a2ea28b3696364fd71
Lora <annelec6@gmail.com>
2026-08-03 23:26:21 +0800

    fix resume download path

--- api/resume.js
+++ api/resume.js
@@ -2,16 +2,16 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = (req, res) => {
-  const filePath = path.join(process.cwd(), "Sample-Resume.pdf");
+  const filePath = path.join(process.cwd(), "public", "Sample-Resume.pdf");
 
   const file = fs.readFileSync(filePath);
+
+  res.setHeader("Content-Type", "application/pdf");
 
   res.setHeader(
     "Content-Disposition",
     "attachment; filename=Sample-Resume.pdf",
   );
-
-  res.setHeader("Content-Type", "application/pdf");
 
   res.status(200).send(file);
 };
